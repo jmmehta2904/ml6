0cdd46e244293ad746d39566041368633d265824
jmmehta2904 <139682925+jmmehta2904@users.noreply.github.com>
2024-11-11 07:38:06 +0530

    Update 7a.c

--- 7a.c
+++ 7a.c
@@ -1,3 +1,10 @@
+#Inter process communication in Linux using following. 
+#A. FIFOS: Full duplex communication between two independent processes. First process accepts 
+sentences and writes on one pipe to be read by second process and second process counts number of 
+#characters, number of words and number of lines in accepted sentences, writes this output in a text file 
+and writes the contents of the file on second pipe to be read by first process and displays on standard 
+output.
+
 //FIFO 1
 
 #include<stdio.h>
