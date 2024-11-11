c80ae8ab4e0f560b7601e780b38536dd2ae4a49d
jmmehta2904 <139682925+jmmehta2904@users.noreply.github.com>
2024-11-11 07:35:18 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,3 +1,7 @@
+#Assignment No. 3: 
+#Implement the C program for CPU Scheduling Algorithms: Shortest Job First 
+(Preemptive) and Round Robin with different arrival time.
+
 #include <stdio.h>
 #include <stdbool.h>
 
